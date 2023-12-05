060d3f327ab6e28c4454223106eac666b5d8a82b
twdent <timo.denton@gmail.com>
2023-12-05 16:40:59 +0100

    Refactor subscriber configuration and image processing for un/compressed images

--- elevation_mapping_cupy/script/elevation_mapping_cupy/elevation_mapping_ros.py
+++ elevation_mapping_cupy/script/elevation_mapping_cupy/elevation_mapping_ros.py
@@ -1,3 +1,5 @@
+import os, sys
+
 from elevation_mapping_cupy import ElevationMap
 from elevation_mapping_cupy import Parameter
 
@@ -14,7 +16,7 @@ import rospkg
 import message_filters
 from cv_bridge import CvBridge
 
-from sensor_msgs.msg import PointCloud2, Image, CameraInfo
+from sensor_msgs.msg import PointCloud2, Image, CameraInfo, CompressedImage
 from grid_map_msgs.msg import GridMap
 from std_msgs.msg import Float32MultiArray
 from std_msgs.msg import MultiArrayLayout as MAL
@@ -22,7 +24,7 @@ from std_msgs.msg import MultiArrayDimension as MAD
 
 import numpy as np
 from functools import partial
-
+import cv2
 PDC_DATATYPE = {
     "1": np.int8,
     "2": np.uint8,
@@ -87,16 +89,20 @@ class ElevationMapWrapper:
         
         for key, config in self.subscribers.items():
             if config["data_type"] == "image":
+
+                #check if compressed
                 
                 
                 self.camera_info_subs[key] = rospy.Subscriber(config["topic_name_camera_info"], CameraInfo, self.callback_camera_info, key, queue_size=1)
                 
-                camera_sub = message_filters.Subscriber(config["topic_name_camera"], Image)
+                camera_sub = message_filters.Subscriber(config["topic_name_camera"], CompressedImage) # changed from Image to CompressedImage
                 # camera_info_sub = message_filters.Subscriber(config["topic_name_camera_info"], CameraInfo)
                 self.image_subs[key] = message_filters.ApproximateTimeSynchronizer(
                     [camera_sub], queue_size=10, slop=0.5
                 )
                 self.image_subs[key].registerCallback(self.image_callback, key)
+
+            
 
 
             elif config["data_type"] == "pointcloud":
@@ -187,8 +193,13 @@ class ElevationMapWrapper:
         t = np.array([t.x, t.y, t.z])
         q = transform.transform.rotation
         R = quaternion_matrix([q.x, q.y, q.z, q.w])[:3, :3]
+        
+        # semantic_img = self.cv_bridge.imgmsg_to_cv2(camera_msg, desired_encoding="passthrough")
+        semantic_img = self.cv_bridge.compressed_imgmsg_to_cv2(camera_msg, desired_encoding="passthrough")
 
-        semantic_img = self.cv_bridge.imgmsg_to_cv2(camera_msg, desired_encoding="passthrough")
+        # #show image
+        # cv2.imshow("image", semantic_img)
+        # cv2.waitKey(1)
 
         if len(semantic_img.shape) != 2:
             semantic_img = [semantic_img[:, :, k] for k in range(3)]
@@ -200,6 +211,7 @@ class ElevationMapWrapper:
         K = np.array(camera_info_msg.K, dtype=np.float32).reshape(3, 3)
         # process pointcloud
         self._map.input_image(sub_key, semantic_img, R, t, K, camera_info_msg.height, camera_info_msg.width)
+
         self._image_process_counter += 1
 
     def pointcloud_callback(self, msg, sub_key):
@@ -277,7 +289,7 @@ class ElevationMapWrapper:
         self.initialize_frame_id = rospy.get_param("~initialize_frame_id", "base")
         self.initialize_tf_offset = rospy.get_param("~initialize_tf_offset", 0.0)
         self.pose_topic = rospy.get_param("~pose_topic", "pose")
-        self.map_frame = rospy.get_param("~map_frame", "map")
+        self.map_frame = rospy.get_param("~map_frame", "odom") # changed this to align with anymal_params.yaml
         self.base_frame = rospy.get_param("~base_frame", "base")
         self.corrected_map_frame = rospy.get_param("~corrected_map_frame", "corrected_map")
         self.initialize_method = rospy.get_param("~initialize_method", "cubic")
